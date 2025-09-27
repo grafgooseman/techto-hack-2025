0577605cd8538855904003dd0dba070c1b3dc871
n3nikita <nikita@bandaruk.com>
2025-09-27 15:49:35 -0400

    Add dotenv and OpenAI dependencies; refactor server.js to use OpenAI API

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import OpenAI from "openai";
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // Use the environment variable
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const response = await client.responses.create({
+  model: "gpt-5",
+  input: "Write a short bedtime story about a unicorn.",
 });
+
+console.log(response);
